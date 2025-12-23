3f1ec77aad7793f8c47b52fc0996d1ee08cb94d1
Kubendiran-dev <kubea.dev@gmail.com>
2025-12-23 12:55:49 +0530

    Added JavaScript practice showcase folder

---
+++ javascript/js demo/demo.js
@@ -0,0 +1,31 @@
+var firstName = "kubea"
+var LastName = "nagaraj"
+var user_name = "kubea01"
+var password = 1234
+var EmailId = "kubea2004@gmail.com"
+var ph_number = 975174548
+var StudentNumber = 21124016
+var courseId = 2001
+var total_student = 60
+var client_name = "mr.x"
+var CompanyLogo = "img"
+var web_tool = "chrome"
+
+{
+    console.log(firstName)
+    console.log(LastName)
+    console.log(user_name)
+    console.log(password)
+    console.log(email)
+    console.log(ph_number)
+    console.log(StudentNumber)
+    console.log(courseId)
+    console.log(total_student)
+    console.log(client_name)
+    console.log(CompanyLogo)
+    console.log(web_tool)
+}
+
+
+
+
